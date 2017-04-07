6a8fc674d8da954b48960ecbf4451332eac1ad04
rknakamura <nakmuraunder@gmail.com>
2017-04-07 11:44:42 -0300

    Add dependecy injection

---
+++ WebApplicationDemo/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WebApplicationDemo
+{
+    public interface IDependencyInjection
+    {
+        string Get();
+    }
+
+    public class DependencyInjection : IDependencyInjection
+    {
+        public string Get()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ WebApplicationDemo/DependencyModule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Ninject.Modules;
+
+namespace WebApplicationDemo
+{
+    public class DependencyModule : NinjectModule
+    {
+        public override void Load()
+        {
+            this.Bind<IDependencyInjection>().To<DependencyInjection>();
+        }
+    }
+}
